e8d054987e8cc1ba9c2c5744c2fd74edbb5c4546
John Whitlock <jwhitlock@mozilla.com>
2020-12-28 12:14:22 -0600

    Add fits_right, ordering

--- puzzle.py
+++ puzzle.py
@@ -1,46 +1,80 @@
 """Solve One Tough Puzzle™"""
 
 from enum import Enum, IntEnum
+from functools import total_ordering
+from itertools import product
 from typing import ClassVar, Dict, Tuple
 
 
-class Shape(Enum):
-    HEART = "Heart"
-    DIAMOND = "Diamond"
-    CLUB = "Club"
-    SPADE = "Spade"
+class OrderedLabeledEnum(bytes, Enum):
+    """An enumeration supporting ordering and a label"""
+
+    def __new__(cls, value, label):
+        obj = bytes.__new__(cls, [value])
+        obj._value_ = value
+        obj.label = label
+        return obj
+
+    def __ge__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value >= other.value
+        return NotImplemented
+
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        return NotImplemented
+
+    def __le__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value <= other.value
+        return NotImplemented
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
 
 
-class End(Enum):
-    TAB = "Tab"
-    BLANK = "Blank"
+class Shape(OrderedLabeledEnum):
+    """Puzzle end shape, in alphabetical order."""
+
+    CLUB = (1, "Club")
+    DIAMOND = (2, "Diamond")
+    HEART = (3, "Heart")
+    SPADE = (4, "Spade")
 
 
-class Side(Enum):
-    RED = "Red"
-    BLACK = "Black"
+class End(OrderedLabeledEnum):
+    """Does the end stick out (tab) or in (blank)?"""
+
+    TAB = (1, "Tab")
+    BLANK = (2, "Blank")
+
+
+class Side(OrderedLabeledEnum):
+    """Is the red or black side up?"""
+
+    RED = (1, "Red")
+    BLACK = (2, "Black")
 
 
 class Turn(IntEnum):
-    NO_TURN = (0,)
-    TURN_90 = (90,)
-    TURN_180 = (180,)
+    NO_TURN = 0
+    TURN_90 = 90
+    TURN_180 = 180
     TURN_270 = 270
+
+
+class Edge(OrderedLabeledEnum):
+    NORTH = (0, "North")
+    EAST = (1, "East")
+    SOUTH = (2, "South")
+    WEST = (3, "West")
 
 
 class Orientation:
     """An orientation of a peice."""
-
-    pip: ClassVar[Dict[Tuple[Shape, End], str]] = {
-        (Shape.HEART, End.TAB): "♥",
-        (Shape.HEART, End.BLANK): "♡",
-        (Shape.DIAMOND, End.TAB): "♦",
-        (Shape.DIAMOND, End.BLANK): "♢",
-        (Shape.CLUB, End.TAB): "♣",
-        (Shape.CLUB, End.BLANK): "♧",
-        (Shape.SPADE, End.TAB): "♠",
-        (Shape.SPADE, End.BLANK): "♤",
-    }
 
     def __init__(
         self,
@@ -55,38 +89,112 @@ class Orientation:
         west_end: End,
     ):
         """Initialize an orientation of a puzzle piece."""
-        self.side = side
-        self.order = [
-            (north_shape, north_end),
-            (east_shape, east_end),
-            (south_shape, south_end),
-            (west_shape, west_end),
-        ]
+        self._attributes = (
+            side,
+            (north_end, east_end, south_end, west_end),
+            (north_shape, east_shape, south_shape, west_shape),
+        )
+
+    @property
+    def side(self):
+        return self._attributes[0]
+
+    @property
+    def ends(self):
+        return self._attributes[1]
+
+    @property
+    def shapes(self):
+        return self._attributes[2]
+
+    @property
+    def edges(self):
+        return tuple(zip(self.shapes, self.ends))
+
+    def end(self, edge: Edge):
+        return self._attributes[1][edge.value]
+
+    def shape(self, edge: Edge):
+        return self._attributes[2][edge.value]
+
+    def edge(self, edge: Edge):
+        return (self._attributes[2][edge.value], self._attributes[1][edge.value])
+
+    north = property(lambda self: self.edge(Edge.NORTH))
+    east = property(lambda self: self.edge(Edge.EAST))
+    south = property(lambda self: self.edge(Edge.SOUTH))
+    west = property(lambda self: self.edge(Edge.WEST))
+    north_shape = property(lambda self: self.shape(Edge.NORTH))
+    east_shape = property(lambda self: self.shape(Edge.EAST))
+    south_shape = property(lambda self: self.shape(Edge.SOUTH))
+    west_shape = property(lambda self: self.shape(Edge.WEST))
+    north_end = property(lambda self: self.end(Edge.NORTH))
+    east_end = property(lambda self: self.end(Edge.EAST))
+    south_end = property(lambda self: self.end(Edge.SOUTH))
+    west_end = property(lambda self: self.end(Edge.WEST))
 
     def __repr__(self):
         """Print a eval() representation."""
-        parts = [self.side]
-        for shape, end in self.order:
+        parts = [str(self.side)]
+        for shape, end in zip(self.shapes, self.ends):
             parts.extend([shape, end])
         return f"{self.__class__.__name__}({', '.join(str(p) for p in parts)})"
 
+    _pip: ClassVar[Dict[Tuple[Shape, End], str]] = {
+        (Shape.HEART, End.TAB): "♥",
+        (Shape.HEART, End.BLANK): "♡",
+        (Shape.DIAMOND, End.TAB): "♦",
+        (Shape.DIAMOND, End.BLANK): "♢",
+        (Shape.CLUB, End.TAB): "♣",
+        (Shape.CLUB, End.BLANK): "♧",
+        (Shape.SPADE, End.TAB): "♠",
+        (Shape.SPADE, End.BLANK): "♤",
+    }
+
     def __str__(self):
-        parts = [self.side.value, "-"]
-        parts.extend([self.pip[edge] for edge in self.order])
+        parts = [self.side.label, "-"]
+        parts.extend([self._pip[edge] for edge in self.edges])
         return "".join(parts)
 
+    def __eq__(self, other):
+        if not isinstance(other, Orientation):
+            return NotImplemented
+        return self._attributes == other._attributes
+
+    def __ne__(self, other):
+        if not isinstance(other, Orientation):
+            return NotImplemented
+        return self._attributes != other._attributes
+
+    def __ge__(self, other: "Orientation"):
+        return self._attributes >= other._attributes
+
+    def __gt__(self, other: "Orientation"):
+        return self._attributes > other._attributes
+
+    def __le__(self, other: "Orientation"):
+        return self._attributes <= other._attributes
+
+    def __lt__(self, other):
+        return self._attributes < other._attributes
+
     def is_valid(self) -> bool:
-        """Determine if the orientation is from One Tough Puzzle."""
-        return True
+        """Determine if the orientation could be from One Tough Puzzle."""
+        return self.ends in (
+            (End.TAB, End.TAB, End.BLANK, End.BLANK),
+            (End.BLANK, End.TAB, End.TAB, End.BLANK),
+            (End.BLANK, End.BLANK, End.TAB, End.TAB),
+            (End.TAB, End.BLANK, End.BLANK, End.TAB),
+        )
 
     def is_standard(self) -> bool:
         """Determine if the orientation is standard."""
-        return self.side == Side.RED and [edge[1] for edge in self.order] == [
+        return self.side == Side.RED and self.ends == (
             End.TAB,
             End.TAB,
             End.BLANK,
             End.BLANK,
-        ]
+        )
 
     def to_standard(self) -> "Orientation":
         if not self.is_valid():
@@ -99,16 +207,16 @@ class Orientation:
 
     def reorient(self, flip: bool = False, turn: Turn = Turn.NO_TURN) -> "Orientation":
         """Turn and flip to new orientation."""
-        new_order = self.order[:]
+        new_edges = list(self.edges)
+        new_side = self.side
 
-        side = self.side
         if flip:
             # Flip left to right
-            new_order = [new_order[0], new_order[3], new_order[2], new_order[1]]
-            if side == Side.RED:
-                side = Side.BLACK
+            new_edges = [new_edges[0], new_edges[3], new_edges[2], new_edges[1]]
+            if self.side == Side.RED:
+                new_side = Side.BLACK
             else:
-                side = Side.RED
+                new_side = Side.RED
 
         index = {
             Turn.NO_TURN: 0,
@@ -116,19 +224,15 @@ class Orientation:
             Turn.TURN_180: -2,
             Turn.TURN_270: -3,
         }[turn]
-        new_order = new_order[index:] + new_order[:index]
+        new_edges = new_edges[index:] + new_edges[:index]
         return Orientation(
-            side, *new_order[0], *new_order[1], *new_order[2], *new_order[3]
+            new_side, *new_edges[0], *new_edges[1], *new_edges[2], *new_edges[3]
         )
-
-    def __eq__(self, other):
-        return (self.order == other.order) and (self.side == other.side)
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
 
 
 class Piece:
+    """A puzzle piece."""
+
     def __init__(
         self,
         north_shape: Shape,
@@ -160,11 +264,84 @@ class Piece:
         ).to_standard()
 
     def __repr__(self):
-        parts = [shape for shape, end in self.orientation.order]
+        parts = [shape for shape, end in self.orientation.edges]
         return f"{self.__class__.__name__}({', '.join(str(p) for p in parts)})"
 
     def __str__(self):
         return str(self.orientation)
+
+    side = property(lambda self: self.orientation.side)
+    north = property(lambda self: self.orientation.north)
+    east = property(lambda self: self.orientation.east)
+    south = property(lambda self: self.orientation.south)
+    west = property(lambda self: self.orientation.west)
+    north_shape = property(lambda self: self.orientation.north_shape)
+    east_shape = property(lambda self: self.orientation.east_shape)
+    south_shape = property(lambda self: self.orientation.south_shape)
+    west_shape = property(lambda self: self.orientation.west_shape)
+    north_end = property(lambda self: self.orientation.north_end)
+    east_end = property(lambda self: self.orientation.east_end)
+    south_end = property(lambda self: self.orientation.south_end)
+    west_end = property(lambda self: self.orientation.west_end)
+
+    def fits_right(self, other: "Piece"):
+        """Return pairs where this fits with the other piece."""
+        combos = product((False, True), Turn, (False, True), Turn)
+        fits = []
+        for flip, turn, other_flip, other_turn in combos:
+            orient = OrientedPiece(self, flip, turn)
+            other_orient = OrientedPiece(other, other_flip, other_turn)
+            if orient.fits_right(other_orient):
+                fits.append((orient, other_orient))
+        return fits
+
+
+class OrientedPiece:
+    """A puzzle piece in a particular orientation."""
+
+    def __init__(self, piece: Piece, flip: bool = False, turn: Turn = Turn.NO_TURN):
+        self.piece = piece
+        self.flip = flip
+        self.turn = turn
+        self.orientation = piece.orientation.reorient(flip=flip, turn=turn)
+
+    def __repr__(self):
+        parts = [repr(self.piece)]
+        if self.flip:
+            parts.append("flip=True")
+        if self.turn:
+            parts.append(f"turn={self.turn!s}")
+
+        return f"OrientedPiece({', '.join(p for p in parts)})"
+
+    def __str__(self):
+        return str(self.orientation)
+
+    def __eq__(self, other):
+        return (
+            (self.piece == other.piece)
+            and (self.flip == other.flip)
+            and (self.turn == other.turn)
+        )
+
+    side = property(lambda self: self.orientation.side)
+    north = property(lambda self: self.orientation.north)
+    east = property(lambda self: self.orientation.east)
+    south = property(lambda self: self.orientation.south)
+    west = property(lambda self: self.orientation.west)
+    north_shape = property(lambda self: self.orientation.north_shape)
+    east_shape = property(lambda self: self.orientation.east_shape)
+    south_shape = property(lambda self: self.orientation.south_shape)
+    west_shape = property(lambda self: self.orientation.west_shape)
+    north_end = property(lambda self: self.orientation.north_end)
+    east_end = property(lambda self: self.orientation.east_end)
+    south_end = property(lambda self: self.orientation.south_end)
+    west_end = property(lambda self: self.orientation.west_end)
+
+    def fits_right(self, other):
+        return (self.east_shape == other.west_shape) and (
+            self.east_end != other.west_end
+        )
 
 
 if __name__ == "__main__":
--- test_puzzle.py
+++ test_puzzle.py
@@ -2,7 +2,33 @@
 
 import pytest
 
-from puzzle import Orientation, Shape, End, Side, Turn, Piece
+from puzzle import Orientation, Shape, End, Side, Turn, Piece, OrientedPiece
+
+
+class TestShape:
+    def test_order(self):
+        assert Shape.CLUB < Shape.DIAMOND < Shape.HEART < Shape.SPADE
+
+    def test_str(self):
+        assert str(Shape.CLUB) == "Shape.CLUB"
+
+    def test_label(self):
+        assert Shape.DIAMOND.label == "Diamond"
+
+
+class TestEnd:
+    def test_order(self):
+        assert End.TAB < End.BLANK
+
+
+class TestSide:
+    def test_order(self):
+        assert Side.RED < Side.BLACK
+
+
+class TestTurn:
+    def test_order(self):
+        assert Turn.NO_TURN < Turn.TURN_90 < Turn.TURN_180 < Turn.TURN_270
 
 
 class TestOrientation:
@@ -29,6 +55,22 @@ class TestOrientation:
             " Shape.HEART, End.TAB, Shape.DIAMOND, End.TAB,"
             " Shape.DIAMOND, End.BLANK, Shape.HEART, End.BLANK)"
         )
+        assert orient.side == Side.RED
+
+        assert orient.north == (Shape.HEART, End.TAB)
+        assert orient.east == (Shape.DIAMOND, End.TAB)
+        assert orient.south == (Shape.DIAMOND, End.BLANK)
+        assert orient.west == (Shape.HEART, End.BLANK)
+
+        assert orient.north_shape == Shape.HEART
+        assert orient.east_shape == Shape.DIAMOND
+        assert orient.south_shape == Shape.DIAMOND
+        assert orient.west_shape == Shape.HEART
+
+        assert orient.north_end == End.TAB
+        assert orient.east_end == End.TAB
+        assert orient.south_end == End.BLANK
+        assert orient.west_end == End.BLANK
 
     def test_rotated_90(self):
         """Test an Orientation rotated 90 from standard orientation"""
@@ -132,18 +174,74 @@ class TestOrientation:
         assert str(flipped) == "Red-♥♦♢♡"
         assert flipped == orient
 
+    def test_invalid(self):
+        invalid = Orientation(
+            Side.RED,
+            Shape.CLUB,
+            End.TAB,
+            Shape.CLUB,
+            End.TAB,
+            Shape.CLUB,
+            End.TAB,
+            Shape.CLUB,
+            End.TAB,
+        )
+        assert not invalid.is_valid()
+
+    def test_order(self):
+        or_least = Orientation(
+            Side.RED,
+            Shape.CLUB,
+            End.TAB,
+            Shape.CLUB,
+            End.TAB,
+            Shape.CLUB,
+            End.TAB,
+            Shape.CLUB,
+            End.TAB,
+        )
+        or_least2 = Orientation(
+            Side.RED,
+            Shape.CLUB,
+            End.TAB,
+            Shape.CLUB,
+            End.TAB,
+            Shape.CLUB,
+            End.TAB,
+            Shape.DIAMOND,
+            End.TAB,
+        )
+        assert or_least < or_least2
+        assert or_least2 > or_least
+
 
 class TestPieces:
     def test_default_init(self):
-        """Default is standard order"""
+        """Default is the standard orientation"""
         piece = Piece(Shape.HEART, Shape.SPADE, Shape.SPADE, Shape.CLUB)
         assert str(piece) == "Red-♥♠♤♧"
         assert repr(piece) == (
             "Piece(Shape.HEART, Shape.SPADE, Shape.SPADE, Shape.CLUB)"
         )
+        assert piece.side == Side.RED
+
+        assert piece.north == (Shape.HEART, End.TAB)
+        assert piece.east == (Shape.SPADE, End.TAB)
+        assert piece.south == (Shape.SPADE, End.BLANK)
+        assert piece.west == (Shape.CLUB, End.BLANK)
+
+        assert piece.north_shape == Shape.HEART
+        assert piece.east_shape == Shape.SPADE
+        assert piece.south_shape == Shape.SPADE
+        assert piece.west_shape == Shape.CLUB
+
+        assert piece.north_end == End.TAB
+        assert piece.east_end == End.TAB
+        assert piece.south_end == End.BLANK
+        assert piece.west_end == End.BLANK
 
     def test_full_init(self):
-        """A non-standard order is converted to standard."""
+        """A non-standard orientation is converted to standard."""
         piece = Piece(
             Shape.SPADE,
             Shape.DIAMOND,
@@ -159,3 +257,160 @@ class TestPieces:
         assert repr(piece) == (
             "Piece(Shape.SPADE, Shape.DIAMOND, Shape.HEART, Shape.DIAMOND)"
         )
+
+    def test_fits_none(self):
+        """A piece with no matches has no fits."""
+        piece1 = Piece(
+            Shape.SPADE,
+            Shape.DIAMOND,
+            Shape.SPADE,
+            Shape.DIAMOND,
+        )
+        piece2 = Piece(
+            Shape.CLUB,
+            Shape.HEART,
+            Shape.CLUB,
+            Shape.HEART,
+        )
+        assert str(piece1) == "Red-♠♦♤♢"
+        assert str(piece2) == "Red-♣♥♧♡"
+        assert piece1.fits_right(piece2) == []
+
+    def test_fit_one_side(self):
+        """A piece with one possible matching side fits 4 ways."""
+        piece1 = Piece(
+            Shape.DIAMOND,
+            Shape.HEART,
+            Shape.HEART,
+            Shape.HEART,
+        )
+        piece2 = Piece(
+            Shape.CLUB,
+            Shape.CLUB,
+            Shape.DIAMOND,
+            Shape.CLUB,
+        )
+        assert str(piece1) == "Red-♦♥♡♡"
+        assert str(piece2) == "Red-♣♣♢♧"
+        assert piece1.fits_right(piece2) == [
+            (
+                OrientedPiece(piece1, turn=Turn.TURN_90),
+                OrientedPiece(piece2, turn=Turn.TURN_90),
+            ),
+            (
+                OrientedPiece(piece1, turn=Turn.TURN_90),
+                OrientedPiece(piece2, flip=True, turn=Turn.TURN_90),
+            ),
+            (
+                OrientedPiece(piece1, flip=True, turn=Turn.TURN_90),
+                OrientedPiece(piece2, turn=Turn.TURN_90),
+            ),
+            (
+                OrientedPiece(piece1, flip=True, turn=Turn.TURN_90),
+                OrientedPiece(piece2, flip=True, turn=Turn.TURN_90),
+            ),
+        ]
+
+
+class TestOrientedPiece:
+    def test_default_init(self):
+        """Default is the standard orientation."""
+        piece = Piece(Shape.SPADE, Shape.DIAMOND, Shape.HEART, Shape.DIAMOND)
+        op = OrientedPiece(piece)
+        assert str(op) == "Red-♠♦♡♢"
+        assert repr(op) == (
+            "OrientedPiece(Piece(Shape.SPADE, Shape.DIAMOND, Shape.HEART, Shape.DIAMOND))"
+        )
+        assert piece.side == Side.RED
+
+        assert piece.north == (Shape.SPADE, End.TAB)
+        assert piece.east == (Shape.DIAMOND, End.TAB)
+        assert piece.south == (Shape.HEART, End.BLANK)
+        assert piece.west == (Shape.DIAMOND, End.BLANK)
+
+        assert piece.north_shape == Shape.SPADE
+        assert piece.east_shape == Shape.DIAMOND
+        assert piece.south_shape == Shape.HEART
+        assert piece.west_shape == Shape.DIAMOND
+
+        assert piece.north_end == End.TAB
+        assert piece.east_end == End.TAB
+        assert piece.south_end == End.BLANK
+        assert piece.west_end == End.BLANK
+
+    def test_flipped_init(self):
+        """A piece can be flipped at init."""
+        piece = Piece(Shape.SPADE, Shape.DIAMOND, Shape.HEART, Shape.DIAMOND)
+        op = OrientedPiece(piece, flip=True)
+        assert str(op) == "Black-♠♢♡♦"
+        assert repr(op) == (
+            "OrientedPiece("
+            "Piece(Shape.SPADE, Shape.DIAMOND, Shape.HEART, Shape.DIAMOND),"
+            " flip=True)"
+        )
+
+    def test_turned_init(self):
+        """A piece can be turned at init."""
+        piece = Piece(Shape.SPADE, Shape.DIAMOND, Shape.HEART, Shape.DIAMOND)
+        op = OrientedPiece(piece, turn=Turn.TURN_90)
+        assert str(op) == "Red-♢♠♦♡"
+        assert repr(op) == (
+            "OrientedPiece("
+            "Piece(Shape.SPADE, Shape.DIAMOND, Shape.HEART, Shape.DIAMOND),"
+            " turn=Turn.TURN_90)"
+        )
+
+    def test_full_init(self):
+        """A piece can be flipped and turned at init."""
+        piece = Piece(Shape.SPADE, Shape.DIAMOND, Shape.HEART, Shape.DIAMOND)
+        op = OrientedPiece(piece, flip=True, turn=Turn.TURN_90)
+        assert str(op) == "Black-♦♠♢♡"
+        assert repr(op) == (
+            "OrientedPiece("
+            "Piece(Shape.SPADE, Shape.DIAMOND, Shape.HEART, Shape.DIAMOND),"
+            " flip=True, turn=Turn.TURN_90)"
+        )
+
+    def test_equality(self):
+        """An OrientedPiece is equal when the same piece and orientation."""
+        piece1 = Piece(Shape.HEART, Shape.DIAMOND, Shape.DIAMOND, Shape.DIAMOND)
+        clone1 = Piece(Shape.HEART, Shape.DIAMOND, Shape.DIAMOND, Shape.DIAMOND)
+        piece2 = Piece(Shape.CLUB, Shape.CLUB, Shape.HEART, Shape.SPADE)
+        assert str(piece1) == "Red-♥♦♢♢"
+        assert str(clone1) == "Red-♥♦♢♢"
+        assert str(piece2) == "Red-♣♣♡♤"
+        assert piece1 != clone1
+        assert piece1 != piece2
+
+        assert OrientedPiece(piece1) == OrientedPiece(piece1)
+        assert OrientedPiece(piece1) != OrientedPiece(clone1)
+        assert OrientedPiece(piece1) != OrientedPiece(piece2)
+
+        assert OrientedPiece(piece1) != OrientedPiece(piece1, flip=True)
+        assert OrientedPiece(piece1) != OrientedPiece(piece1, turn=Turn.TURN_90)
+        assert OrientedPiece(piece1) != OrientedPiece(
+            piece1, flip=True, turn=Turn.TURN_180
+        )
+
+        assert OrientedPiece(piece1, flip=True) == OrientedPiece(piece1, flip=True)
+        assert OrientedPiece(piece1, turn=Turn.TURN_90) == OrientedPiece(
+            piece1, turn=Turn.TURN_90
+        )
+        assert OrientedPiece(piece1, flip=True, turn=Turn.TURN_180) == OrientedPiece(
+            piece1, flip=True, turn=Turn.TURN_180
+        )
+
+    @pytest.mark.parametrize(
+        "flip1,turn1,flip2,turn2,fits",
+        [
+            (False, Turn.NO_TURN, False, Turn.NO_TURN, False),
+            (False, Turn.TURN_90, False, Turn.TURN_90, True),
+        ],
+    )
+    def test_fits_right(self, flip1, turn1, flip2, turn2, fits):
+        """fits_right() returns True when oriented to fit."""
+        piece1 = Piece(Shape.HEART, Shape.DIAMOND, Shape.DIAMOND, Shape.DIAMOND)
+        piece2 = Piece(Shape.CLUB, Shape.CLUB, Shape.HEART, Shape.SPADE)
+        op1 = OrientedPiece(piece1, flip1, turn1)
+        op2 = OrientedPiece(piece2, flip2, turn2)
+        assert op1.fits_right(op2) is fits
